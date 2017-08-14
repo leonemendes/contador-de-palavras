09b4cc507c82d751aced2860b3f2087bb12d1f18
leonemendes <leonemendes23@gmail.com>
2017-08-13 22:21:09 -0300

    Working code

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 /* Contador de palavras
- *
+ * Leone Almendro Mendes
+ * RA: 172125
  * Este programa recebera uma serie de caracteres representando palavras em sua
  * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
  * numero de palavras separadas que recebeu e, apos, encerrar.
@@ -9,14 +10,28 @@
 
 int main() {
 
-  char c;
+  char c, last, lastbutone;
+  int words = 0;
 
   c = 1;
+  last = 1;
+  lastbutone = 1;
 
   while (c != '\n') {
     scanf("%c", &c);
+    if(c == ' ' || c == '.' || c == ',' || c == '!' || c == '?' || c == '-' || c == ';' || c == ':' || c == '\n'){
+      
+      if((last >= 0x41 && last <= 0x5A) || (last >= 0x61 && last <= 0x7A) || (last >= 0x30 && last <= 0x39)){
+	words ++;
+      }
+    }
+    if(((lastbutone) >= 0x30 && (lastbutone) <= 0x39) && (last == '.' || last == ',') && ((c >= 0x30) && (c <= 0x39))){
+      words --;
+    }
+    lastbutone = last;
+    last = c;
   }
 
-  printf("1\n");
+  printf("%d\n", words);
   return 0;
 }
